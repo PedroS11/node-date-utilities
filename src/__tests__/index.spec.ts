9b3c01acf3d84ca4dbdea1c223a6fdd3be6b7f59
Pedro Oliveira Silva <psilva@dunelm.com>
2020-06-10 23:13:35 +0100

    Added jest to repo, first test

---
+++ src/__tests__/index.spec.ts
@@ -0,0 +1,7 @@
+import {sum} from "..";
+
+describe("index", () => {
+    it("should return the sum", () => {
+        expect(sum(1,2)).toBe(3);
+    })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export const testFunc = () => {
-  console.log("oi");
+export const sum = (a: number, b: number): number => {
+  return a + b;
 };
